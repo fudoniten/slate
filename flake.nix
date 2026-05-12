39d9567557d131236d4098fff002779a1e6e44d8
niten <niten@fudo.org>
2026-05-12 11:01:07 -0700

    Add deployContainer app to flake.nix following Pseudovision pattern
    - Add nix-helpers input for deployContainers helper
    - Convert deployContainer package to use helpers.deployContainers
    - Add automatic versioning with git timestamps (YYYYMMDD format)
    - Add deployContainer app for easy deployment with 'nix run .#deployContainer'
    - Push to registry.kube.sea.fudo.link with both 'latest' and version tags
    - Include environment variables for NODE_ENV, GIT_COMMIT, GIT_TIMESTAMP, VERSION_TAG

--- flake.nix
+++ flake.nix
@@ -1,29 +1,26 @@
 {
-  description = "Slate - Kubernetes-ready ClojureScript UI service for Pseudovision/Tunarr ecosystem";
+  description =
+    "Slate - Kubernetes-ready ClojureScript UI service for Pseudovision/Tunarr ecosystem";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-helpers = {
+      url = "github:fudoniten/fudo-nix-helpers";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-helpers }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        helpers = nix-helpers.legacyPackages.${system};
         jdk = pkgs.jdk17;
         nodejs = pkgs.nodejs_20;
         packageJson = builtins.fromJSON (builtins.readFile ./package.json);
-      in
-      {
+      in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            jdk
-            nodejs
-            clojure
-            git
-            curl
-            docker
-          ];
+          buildInputs = with pkgs; [ jdk nodejs clojure git curl docker ];
 
           shellHook = ''
             echo "Slate development environment loaded"
@@ -38,12 +35,7 @@
           tag = "latest";
           maxLayers = 120;
 
-          contents = with pkgs; [
-            cacert
-            nodejs_20
-            bash
-            coreutils
-          ];
+          contents = with pkgs; [ cacert nodejs_20 bash coreutils ];
 
           config = {
             Env = [
@@ -53,19 +45,21 @@
               "GIT_TIMESTAMP=${toString self.lastModified}"
             ];
             WorkingDir = "/app";
-            ExposedPorts = {
-              "3000/tcp" = { };
-            };
+            ExposedPorts = { "3000/tcp" = { }; };
             Entrypoint = [ "${nodejs}/bin/node" "server.js" ];
             User = "nobody";
           };
 
-          extraCommands = ''
-            mkdir -p app/resources/public/js
-          '';
+          default = self.packages.${system}.deployContainer;
         };
 
-        defaultPackage = self.packages.${system}.deployContainer;
-      }
-    );
+        apps = {
+          deployContainer = {
+            type = "app";
+            program =
+              let deployContainer = self.packages."${system}".deployContainer;
+              in "${deployContainer}/bin/deployContainers";
+          };
+        };
+      });
 }
